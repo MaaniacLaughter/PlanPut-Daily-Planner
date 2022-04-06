2f84ffd8640c98e8d4a130c692126944bb954fc9
MaaniacLaughter <maanijacob24@gmail.com>
2022-04-06 15:02:03 +0530

    Set up reading from a file with entry numbers/dates

--- planPut.c
+++ planPut.c
@@ -1,13 +1,23 @@
 #include<stdio.h>
 #include<string.h>
 
+
+typedef struct entry
+{
+    int n;
+    char date[12];
+}entry;
+
 int checkUser(char [],char []);
 void encrypt(char* );
 void decrypt(char*, char* );
+void readHandler( FILE*, entry*);
 
 int main()
 {
-    /*char s[] = "Maani";       Initial Setup to encrypt file manually. will change later. Must.
+    /*char x = 'a';
+    printf("%c\n", x+200000009);*/ //To check Possible encryptions
+    /*char s[] = "password";       //Initial Setup to encrypt file manually. will change later. Must.
     encrypt(s);*/
     char us[20];
     char pw[10];
@@ -17,13 +27,48 @@ int main()
     scanf("%s",&pw);
     if(checkUser(us,pw)==1)
     {
-        printf("Valid User, Welcome to your Inner Sanctum");
+        printf("\nValid User, Welcome to your Inner Sanctum");
     }
     else
     {
-        printf("new phone who dis");
+        printf("\nnew phone who dis");
+        return 0;
     }
+    FILE* f1 = fopen("fileHandler.txt", "r");
+    entry top;
+    readHandler(f1, &top);
+    if(top.n==0)
+    {
+        printf("\nNo entries yet. ");
+    }
+    else
+    {
+        printf("\nThe last entry (number %d) was dated %s. ", top.n, top.date);
+    }
+    printf("\nWould you like to create a new entry for today? Or want to view an older entry?");
+
     return 0;
+}
+
+void readHandler(FILE* f1, entry* e)
+{
+    int n1;
+    int n2;
+    char buf[50];
+    fscanf(f1,"%d\n",&n1);
+    if(n1==0)
+    {
+        e->n = 0;
+    }
+    else
+    {
+        //printf("%d", n1);
+        fscanf(f1, "%d %s\n",&n2, &buf);
+        strcpy(e->date,buf);
+        e->n = n2;
+        //printf("\n%d %s", e->n, e->date);
+
+    }
 }
 
 int checkUser(char u[],char p[])
@@ -31,18 +76,18 @@ int checkUser(char u[],char p[])
     FILE* f1 = fopen("Login.txt","r");
     char s1[2][20],s[20];
     int i = 0;
-    //fscanf(f1,"%s\n%s",&s1,&s2);
-    while(fgets(s,20,f1)!=NULL && i<2)
+    fscanf(f1,"%s\n%s",&s1[0],&s1[1]);
+    /*while(fgets(s,20,f1)!=NULL && i<2)
     {
         printf("\n%s", s);
         //printf("Hello");                //Debugging
         strcpy(s1[i],s);
         i++;
-    }
-    printf("\nêyïïÅçè|");
+    }*/
+    //printf("\nêyïïÅçè|");
     //fgets(s2,10,f1);
-    printf("%s",s1[0]);                   //Error Checking
-    printf("\n%s",s1[1]);
+    //printf("%s",s1[0]);                   //Error Checking
+    //printf("\n%s",s1[1]);
     decrypt(s1[0],s1[0]);
     decrypt(s1[1],s1[1]);
     /*printf("%s",s1[0]);                 //Error Checking
@@ -55,12 +100,13 @@ int checkUser(char u[],char p[])
 void encrypt(char *s)
 {
     int l = strlen(s);
-    printf("\n%d", l);
+    //printf("\n%d", l);
     for(int i = 0; i<l; i++)
     {
-        s[i] = s[i]+(3*l)%256;
+        //s[i] = s[i]+(3*l)%4;
+        s[i] = s[i]+i;
     }
-    printf("\n%s",s);
+    printf("%s\n",s);
 }
 
 void decrypt(char *s, char* res)
@@ -68,6 +114,8 @@ void decrypt(char *s, char* res)
     int l = strlen(s);
     for(int i = 0; i<l; i++)
     {
-        res[i] = s[i]-(3*l)%256;
+        //res[i] = s[i]-(3*l)%4;
+        res[i] = s[i]-i;
     }
+    //printf("%s\n",res);
 }                                       
